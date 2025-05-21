19844c08bb366871e28c0b6a3f4e47c317fe3a1f
gyfen <yifanchen04@gmail.com>
2025-05-21 21:08:19 +0200

    selectacle materials, opactiy

--- Assets/Scripts/Instance.js
+++ Assets/Scripts/Instance.js
@@ -1,12 +1,20 @@
+// @input Asset.Material[] materials
+
 // assigned by instance controller
 let nutriScore;
 
 const store = global.persistentStorageSystem.store;
+const renderMeshVisual = script.sceneObject.getComponent("Component.RenderMeshVisual");
 
 function updateMaterial() {
-    if (nutriScore < store.getInt("nutriScore")) {
-        // diminish
-    }
+    // assign new material
+    const newMaterial = script.materials[store.getInt("overlayType")].clone();
+
+    const alpha = nutriScore < store.getInt("nutriScore") ? 0 : 0.5;
+    const color = newMaterial.mainPass.baseColor;
+    newMaterial.mainPass.baseColor = new vec4(color.x, color.y, color.z, alpha);
+
+    renderMeshVisual.mainMaterial = newMaterial;
 }
 
 script.updateMaterial = updateMaterial;
