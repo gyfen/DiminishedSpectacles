d4553ce3eb9dacb1925f0436edf38ca814b64025
gyfen <yifanchen04@gmail.com>
2025-05-21 18:43:10 +0200

    init

---
+++ Assets/Scripts/Instance.js
@@ -0,0 +1,15 @@
+let info;
+
+function updateMaterial() {
+    //    pull settings from persistent storage
+    //
+    print("testing");
+}
+
+// function onAwake() {
+// updateMaterial();
+// }
+
+// onAwake();
+
+script.updateMaterial = updateMaterial;
---
+++ Assets/Scripts/InstanceController.js
@@ -0,0 +1,115 @@
+/*
+Detection Controller
+*/
+
+/* Inputs */
+// @input Component.ScriptComponent MLController
+
+// @ui {"widget" : "separator"}
+// @input Component.DeviceTracking deviceTracking
+
+// @ui {"widget" : "separator"}
+// @input Asset.ObjectPrefab prefab
+
+// @ui {"widget" : "separator"}
+// @input float screenScalingX = 1.0
+// @input float screenScalingY = 1.0
+
+// @ui {"widget" : "separator"}
+// TODO: remove debug
+// @input bool debug
+// @input Asset.Texture testingImage { "showIf" : "debug"}
+
+/* Variables */
+let cameraModule;
+let imageRequest;
+
+if (!script.debug) {
+    cameraModule = require("LensStudio:CameraModule");
+    imageRequest = CameraModule.createImageRequest();
+    // imageRequest.crop(Rect.create(-1, 1, -1, 1));
+    // imageRequest.resolution(new vec2(1280, 1280));
+}
+
+/* Transform screenpoint to correcly fit to the spectacles screen */
+function transformScreenPoint(screenPoint) {
+    return new vec2(
+        screenPoint.x * script.screenScalingX - (script.screenScalingX - 1) / 2,
+        screenPoint.y * script.screenScalingY - (script.screenScalingY - 1) / 2
+    );
+}
+
+/* Spawn a prefab */
+function spawnDiminished(screenPos, label, confidence, info) {
+    const results = script.deviceTracking.hitTestWorldMesh(screenPos);
+
+    if (results.length == 0) {
+        return false;
+    }
+
+    // Get World Mesh data at the screen position
+    const point = results[0].position;
+    const normal = results[0].normal;
+
+    // Instantiate the object we want to place
+    const instance = script.prefab.instantiate(script.getSceneObject());
+    const instanceScript = instance.getComponent("Component.ScriptComponent");
+
+    // Update the instance
+    instanceScript.info = info;
+    instanceScript.updateMaterial();
+
+    // TODO: they should all have to same normal
+    // Rotate the object based on World Mesh Surface
+    const up = vec3.up();
+    const forwardDir = up.projectOnPlane(normal);
+    const rot = quat.lookAt(forwardDir, normal);
+    instance.getTransform().setWorldRotation(rot);
+
+    // Set position
+    instance.getTransform().setWorldPosition(point);
+
+    return true;
+}
+
+/*  Triggers when detections are updated.
+    Spawns prefabs for all
+*/
+function parseDetections(detections) {
+    for (let i = 0; i < detections.length; i++) {
+        const { label, score, bbox } = detections[i];
+        // TODO: make this better
+        spawnDiminished(transformScreenPoint(new vec2(bbox[0], bbox[1])), label, confidence, info);
+    }
+}
+
+function calibrate() {
+    mlController.runOnce();
+}
+
+function onDetectionsUpdated(result) {
+    // Delete all existing instances
+    const sceneObj = script.getSceneObject();
+    for (let i = 0; i < sceneObj.getChildrenCount(); i++) {
+        const instance = sceneObj.getChild(i);
+        instance.destroy();
+    }
+
+    parseDetections(result);
+
+    calibrate();
+}
+
+function updateInstances() {
+    const sceneObj = script.getSceneObject();
+    for (let i = 0; i < sceneObj.getChildrenCount(); i++) {
+        const instance = sceneObj.getChild(i);
+        const instanceScript = instance.getComponent("Component.ScriptComponent");
+        instanceScript.updateMaterial();
+    }
+}
+
+// script.calibrate = calibrate;
+
+// TODO: temp test
+await calibrate();
