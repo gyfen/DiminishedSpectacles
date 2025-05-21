f32a9863ea1611f292c7640db314b5d5c44cabcd
gyfen <yifanchen04@gmail.com>
2025-05-21 21:56:42 +0200

    remove hardcoded nutriscore

--- Assets/Scripts/Instance.js
+++ Assets/Scripts/Instance.js
@@ -7,8 +7,7 @@ function updateMaterial() {
     // assign new material
     const newMaterial = script.materials[store.getInt("overlayType")].clone();
 
-    // nutriscore is assigned by instance controller
-    const alpha = script.nutriScore < 5 ? 0 : 1;
+    const alpha = script.nutriScore < store.getInt("NutriScore") ? 0 : 1; // nutriscore is assigned by instance controller
     const color = newMaterial.mainPass.baseColor;
     newMaterial.mainPass.baseColor = new vec4(color.x, color.y, color.z, alpha);
 
